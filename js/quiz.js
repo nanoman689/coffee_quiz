657f762444097f646bc3d3c68d79537f9350f10d
Nanoman <nanoman689@gmail.com>
2015-03-23 20:53:14 -0700

    Starting up the Coffee Quiz
    - Non-interactive version of the site.
    - Getting the look down the way I want it to look.
    - Making sure buttons are being clicked.

---
+++ js/quiz.js
@@ -0,0 +1,14 @@
+$(document).ready(function (){
+    $(".1").click(function() {
+        $( this ).toggleClass( "highlight" );
+    });
+    $(".2").click(function() {
+        $( this ).toggleClass( "highlight" );
+    });
+    $(".3").click(function() {
+        $( this ).toggleClass( "highlight" );
+    });
+    $(".4").click(function() {
+        $( this ).toggleClass( "highlight" );
+    });
+});
