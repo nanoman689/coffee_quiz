059f21f001afc9ab4fd376237bcc88497504b03c
Nanoman <nanoman689@gmail.com>
2015-04-04 18:13:31 -0700

    Coffee Quiz is done!
    Yes, it actually works!

--- js/coffee_quiz.js
+++ js/coffee_quiz.js
@@ -1,5 +1,6 @@
 var questions = [];
 var currentQuestion = 0;
+var points = 0;
 
 /* Question object */
 
@@ -16,36 +17,63 @@ function Question() {
   }
 }
 
-/* question 1 */
+/* questions and answers */
 
 function init (){
+
   var question1 = new Question();
 
   var question2 = new Question();
 
-  question1.text = "Is this a question?";
-  question1.answers.push("Answer 1");
-  question1.answers.push("Answer 2");
-  question1.answers.push("Answer 3");
-  question1.answers.push("Answer 4");
-  question1.correctAnswer = 3;
+  var question3 = new Question();
+
+  var question4 = new Question();
+
+  question1.text = "What is an Americano?";
+  question1.answers.push("1. Hot Water and espresso");
+  question1.answers.push("2. Warm milk and coffee");
+  question1.answers.push("3. Milk, water and espresso");
+  question1.answers.push("4. Hot water, cream and espresso");
+  question1.correctAnswer = 0;
   question1["pointValue"] = 10;
 
-  question2.text = "Is this a question2?";
-  question2.answers.push("Answer 1b");
-  question2.answers.push("Answer 2b");
-  question2.answers.push("Answer 3b");
-  question2.answers.push("Answer 4b");
+  question2.text = "What is an Ristretto?";
+  question2.answers.push("1. Warm milk and coffee");
+  question2.answers.push("2. Espresso with whip cream");
+  question2.answers.push("3. Normal espresso, but with half the amount of water.");
+  question2.answers.push("4. Coffee with a shot of espresso");
   question2.correctAnswer = 2;
   question2["pointValue"] = 10;
 
+  question3.text = "What is a Flat white?";
+  question3.answers.push("1. Espresso with a similar proportion of coffee to milk as a latte and a cappuccino");
+  question3.answers.push("2. Latte with whip cream and coffee");
+  question3.answers.push("3. Coffee and warm milk");
+  question3.answers.push("4. Espresso with cream");
+  question3.correctAnswer = 0;
+  question3["pointValue"] = 10;
+
+  question4.text = "What is a Eiskaffee?";
+  question4.answers.push("1. Coffee and ice");
+  question4.answers.push("2. Espresso with steamed milk");
+  question4.answers.push("3. Espresso with half milk and half cream");
+  question4.answers.push("4. Chilled coffee, milk, vanilla ice cream");
+  question4.correctAnswer = 3;
+  question4["pointValue"] = 10;
+
   questions.push(question1);
   questions.push(question2);
+  questions.push(question3);
+  questions.push(question4);
+
+  /* push the question to the DOM */
 
   document.getElementById('questionTop').innerHTML = questions[currentQuestion].text;
   
   var ans = "";
-    
+  
+  /* set up the questions in order and displays them*/
+
   for (var i = 0; i < questions[currentQuestion].answers.length; i++){
       ans += "<p>" + "<input type = 'radio' name = 'choose' value = '"+i+"'>" + questions[currentQuestion].answers[i] + "</p>";
     }
@@ -54,30 +82,26 @@ function init (){
 
 }
 
-/* number of questions is how many?
-
-var numQuestions = questions.length;`
-*/
-
-
-
-/* get the current answer
-
-var q = questions[currentQuestion];
-
-Put the Answers and Questions on screen */
-
 init();
 
 $(document).ready(function (){
+  pointValue = 0;
 
   $('#chooseGuess').click(function(){
     var choosen = $( "input[name='choose']:checked");
     if (choosen.val() == questions[currentQuestion].correctAnswer){
       $('#result').html("That's Correct!");
+      pointValue = pointValue + 10;
+      $('.coffeeCup h6').html(pointValue);
+
+      /* document.getElementById('coffeeCup').innerHTML = "Your score is now " + pointValue; */
+
+      console.log(pointValue);
+
     } else {
       $('#result').html("Nope, that is not right!");
     }  
+
   });
   $('#chooseNext').click(function(){
     currentQuestion = currentQuestion + 1;
@@ -95,7 +119,8 @@ $(document).ready(function (){
 
     } else{
 
-      $('#result').html('Thanks for playing!');
+      $('#result').html('Thanks for playing! Your final score was ' + pointValue + " out of a possible 40!");
+
 
     }
   });
